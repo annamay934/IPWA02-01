cc503a8648762739210513ef8fb6a40e8f3d1356
anna934 <anna.victoriamay@web.de>
2023-10-31 07:29:41 +0100

    sending data to database and request data from database

--- GhostFishingNetAdministration/src/main/java/example/Main.java
+++ GhostFishingNetAdministration/src/main/java/example/Main.java
@@ -1,6 +1,9 @@
 package example;
 
-import example.myapp.NaturalPerson;
+import example.myapp.model.GhostFishingNet;
+import example.myapp.model.ReportingPerson;
+import example.myapp.model.RescuingPerson;
+import example.myapp.model.Status;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -12,13 +15,61 @@ public class Main {
         EntityManagerFactory factory = Persistence.createEntityManagerFactory("GhostFishingNetProject");
         EntityManager em = factory.createEntityManager();
 
-        NaturalPerson p = new NaturalPerson();
-        p.setFirstName("Anna");
-        p.setPhoneNumber("01742536217");
-        p.setLastName("May");
+        //new reporting Person with ghost fishing net 1, 2, 3
+        ReportingPerson rep = new ReportingPerson();
+        rep.setFirstName("Erika");
+        rep.setPhoneNumber("01742536678");
+        rep.setLastName("Musterfrau");
+
+        GhostFishingNet g1 = new GhostFishingNet();
+        g1.setGfnLocationLatitude(25.678);
+        g1.setGfnLocationLongitude(124.456);
+        g1.calculatedGfnEstimatedSize(3,1);
+        g1.addStatus();
+
+        g1.getStatus();
+        g1.setGfnReportDate();
+
+        rep.addGhostFishingNet(g1);
+
+        //new rescuing person with ghost fishing net g2
+        RescuingPerson res = new RescuingPerson();
+        res.setFirstName("Maximilian");
+        res.setPhoneNumber("01742536678");
+        res.setLastName("Mustermann");
+        res.setUserName("Max");
+        res.setPassword("Mustermann");
+
+        GhostFishingNet g2 = new GhostFishingNet();
+        g2.setGfnLocationLatitude(26.345);
+        g2.setGfnLocationLongitude(45.123);
+        g2.calculatedGfnEstimatedSize(1,2);
+        Status status2 = new Status();
+        status2.setGfnStatusRescued(true);
+        g2.setGfnStatus(status2);
+        g2.setGfnReportDate();
+        g2.setGfnRescueDate();
+
+        rep.addGhostFishingNet(g2);
+        res.setRescuingGfn(g2);
+
+        GhostFishingNet g3 = new GhostFishingNet();
+        g3.setGfnLocationLatitude(34.567);
+        g3.setGfnLocationLongitude(132.789);
+        g3.calculatedGfnEstimatedSize(2,4);
+        Status status3 = new Status();
+        status3.setGfnStatusReported(true);
+        g3.setGfnStatus(status3);
+        g3.setGfnReportDate();
+
+        rep.addGhostFishingNet(g3);
 
         em.getTransaction().begin();
-        em.persist(p);
+        em.persist(rep);
+        em.persist(res);
+        em.persist(g1);
+        em.persist(g2);
+        em.persist(g3);
         em.getTransaction().commit();
         em.close();
         factory.close();
